22dc783cf7c4f87351b16e471f170bfadd38d2ba
antraxalan <aruquipa.alan@gmail.com>
2016-03-21 21:40:25 -0400

    21/03/2016

--- sitrans_insert.php
+++ sitrans_insert.php
@@ -23,6 +23,10 @@ $fecha=date('d/m/Y');
 $fecha=date('d/m/Y').' '.date('h:i:s a');
 // $fecha='22/08/2013';
 
+// $cont=$info.' - '.$data;
+// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
+// $datos->execute(array(':id' => 1, ':cont' => $cont, ':fecha' => $fecha));
+
 
 
 switch ($info) {
@@ -45,77 +49,16 @@ switch ($info) {
 
 
 
-	// case "articulo":
-	// $verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
-	// if($verificar > 0)
-	// {
-	// 	$datos = $db->prepare("SELECT a.codmarca codmarca,desmarca,a.CodArt CodArt,a.DesArt DesArt,a.DesArtReducido DesArtReducido,a.Calibre Calibre,
-	// 		a.TipoArticulo TipoArticulo,a.CantxEmpaque CantxEmpaque,a.PrecioCompra PrecioCompra,a.PrecioVtaMin PrecioVtaMin,a.PrecioVtaMax PrecioVtaMax,
-	// 		a.CodBotella CodBotella,b.DesArt DesBotella,b.PreciovtaMin PVtaMinBot,a.CodCaja CodCaja,c.Desart DesCaja,
-	// 		c.PreciovtaMin PVtaMinCaja,c.PreciovtaMax PVtaMaxCaja,a.estado
-	// 		from articulo a inner join marca d on a.codmarca=d.codmarca
-	// 		left outer join articulo b on a.CodBotella=b.CodArt
-	// 		left outer join articulo c on a.CodCaja=c.CodArt
-	// 		where a.estado='A'
-	// 		order by a.codmarca,a.codart");
-	// 	$datos->execute();
-
-	// 	$articulos='';
-	// 	while($row = $datos->fetch()) {
-	// 		for ($i=0; $i < 19; $i++) { 
-	// 			$articulos.=$row[$i].'@';
-	// 		}
-	// 	}
-	// 	$array  = substr($articulos, 0, -1);
-	// 	$array1 = explode("@", $array);
-	// 	$array2 = array_chunk($array1, 19);
-	// 	// echo "<pre>";
-	// 	// print_r($array2);
-	// 	// echo "</pre>";
-	// 	// echo($array2);
-	// 	echo json_encode($array2);
-	// }
-	// break;
-
-
-
-
-
-	// case "cliente":
-	// $verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
-	// if($verificar > 0)
-	// {
-	// 	$datos = $db->prepare("SELECT a.codcliente codcliente,nombre,razonsocial,direccion,nit,nrotelefono1,nrotelefono2,a.codzona codzona,deszona,a.codpersonal,despersonal,d.codruta codruta,desruta
-	// 		from clientes a inner join zona b on a.codzona=b.codzona
-	// 		inner join personal c on a.codpersonal=c.codpersonal
-	// 		left outer join RutasClientes d on a.codcliente=d.codcliente
-	// 		left outer join Rutas e on d.codruta=e.codruta
-	// 		where a.codcliente>=100");
-	// 	$datos->execute();
-
-	// 	$articulos='';
-	// 	while($row = $datos->fetch()) {
-	// 		for ($i=0; $i < 13; $i++) { 
-	// 			$articulos.=$row[$i].'@';
-	// 		}
-	// 	}
-	// 	$array  = substr($articulos, 0, -1);
-	// 	$array1 = explode("@", $array);
-	// 	$array2 = array_chunk($array1, 13);
-
-	// 	echo json_encode($array2);
-	// }
-	// break;
-
-
-
 
 
 	case "detalle":
+	// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
+	// $datos->execute(array(':id' => 1, ':cont' => 1, ':fecha' => $fecha));
+
 	$verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
 	if($verificar > 0)
 	{
-		$id = $db->query("SELECT max(id) FROM probando")->fetchColumn();
+		$id = $db->query("SELECT max(CodSincDetalle) FROM SincDetalle")->fetchColumn();
 		if($id == ''){
 			$id = 1;
 		}else{
@@ -123,94 +66,58 @@ switch ($info) {
 		}
 
 		$cont=0;
-		// $data=$data;
-
-		// $tipo_riesgo = $db->prepare('SELECT id_rtype ,detalle from risktype ORDER BY id_rtype DESC');
-		// $tipo_riesgo->execute();
-
-		// while($row = $data->fetch()) {  
-		// 	$cont=$cont+1;
-		// }
-// $cont=gettype($data);
+		// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
+		// $datos->execute(array(':id' => $id, ':cont' => $cont, ':fecha' => $fecha));
 		$array=$data;
 		$array  = substr($array, 0, -1);
 		$array1 = explode("|", $array);
-		$array2 = array_chunk($array1, 18);
+		if( !empty( $array1 ) ){
+			$array2 = array_chunk($array1, 18);
+			// $cont=count($array2);
+			foreach ($array2 as $value) {
 
-		foreach ($array2 as $value) {
-			
-			$aux=explode(' ', $value[3]);
-			$value[3]=$aux[0];
-			$value[3]=implode('-', array_reverse(explode('-', $value[3])));
+				$aux=explode(' ', $value[3]);
+				$value[3]=$aux[0];
+				$value[3]=implode('-', array_reverse(explode('-', $value[3])));
 
-			$aux=explode(' ', $value[4]);
-			$value[4]=$aux[0];
-			$value[4]=implode('-', array_reverse(explode('-', $value[4])));
+				$aux=explode(' ', $value[4]);
+				$value[4]=$aux[0];
+				$value[4]=implode('-', array_reverse(explode('-', $value[4])));
 
-			// $cont=$value[3];
-			// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
-			// $datos->execute(array(':id' => $id, ':cont' => $cont, ':fecha' => $value[4]));
+				// $cont=$value[3];
+				// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
+				// $datos->execute(array(':id' => $id, ':cont' => $cont, ':fecha' => $fecha));
 
-			// $datos = $db->prepare('INSERT INTO probando_detalle VALUES(:TipoDcto, :NroDcto, :Apu, :Fecha, :FechaVto, :TipoDctoM, :NroDctoM, :Precio, :Tc, :CodConcepto, :CodCliente, :Debe, :Haber, :CodArt, :Dcajas, :Hcajas, :Dunidades, :Hunidades)');
-			// $datos->execute(array('TipoDcto' => $value[0], 'NroDcto' => $value[1], 'Apu' => $value[2], 'Fecha' => $value[3], 'FechaVto' => $value[4], 'TipoDctoM' => $value[5], 'NroDctoM' => $value[6], 'Precio' => $value[7], 'Tc' => $value[8], 'CodConcepto' => $value[9], 'CodCliente' => $value[10], 'Debe' => $value[11], 'Haber' => $value[12], 'CodArt' => $value[13], 'Dcajas' => $value[14], 'Hcajas' => $value[15], 'Dunidades' => $value[16], 'Hunidades' => $value[17]));
+				$datos = $db->prepare('INSERT INTO SincDetalle VALUES(:CodSincDetalle,:TipoDcto, :NroDcto, :Apu, :Fecha, :FechaVto, :TipoDctoM, :NroDctoM, :Precio, :Tc, :CodConcepto, :CodCliente, :Debe, :Haber, :CodArt, :Dcajas, :Hcajas, :Dunidades, :Hunidades, :FechaCarga)');
+				$datos->execute(array('CodSincDetalle' => $id,'TipoDcto' => $value[0], 'NroDcto' => $value[1], 'Apu' => $value[2], 'Fecha' => $value[3], 'FechaVto' => $value[4], 'TipoDctoM' => $value[5], 'NroDctoM' => $value[6], 'Precio' => $value[7], 'Tc' => $value[8], 'CodConcepto' => $value[9], 'CodCliente' => $value[10], 'Debe' => $value[11], 'Haber' => $value[12], 'CodArt' => $value[13], 'Dcajas' => $value[14], 'Hcajas' => $value[15], 'Dunidades' => $value[16], 'Hunidades' => $value[17], 'FechaCarga' => $fecha));
+
+			}
 		}
-
 		$cont=count($array2);
 
-		// $d = [1, 'alan', '23-05-2016'];
-		// $data = array_fill(0, 2, $d);
-		// $verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
-		// $query = "SELECT * FROM table";
-		// $sth = $db->query($query);
-		// $result = $sth->fetchAll();
 
-		// $query="INSERT INTO probando ( id, cont, fecha ) VALUES";
-		// 	// "INSERT INTO MyTable ( Column1, Column2 ) VALUES
-		// 	// ( Value1, Value2 ), ( Value1, Value2 )"
-		$len=10000;
+
+
+		
+
+		// $query="INSERT INTO probando ( id, cont, fecha )";
+
 		// for ($i=0; $i < $len; $i++) { 
-		// 		// $query .= " ('$id','$i','$fecha')," 
-		// 	$datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
-		// 	$datos->execute(array(':id' => $i, ':cont' => '2', ':fecha' =>'01/01/1999' ));
+		// 	if(($i+1)!=$len){
+		// 		$query.=" SELECT $i,'2','01/01/1999' UNION ALL ";
+		// 	}else{
+		// 		$query.=" SELECT $i,'2','01/01/1999'";				
+		// 	}
+
 		// }
-		// 	$query  = substr($query, 0, -1);
-		// 	// $query="INSERT INTO probando ( id, cont, fecha ) VALUES";
-		// 	$sth = $db->query($query);
-		// $result = $sth->fetchAll();
-		// $i=5;
-
-
-		 // $db->query("INSERT INTO probando ( id, cont, fecha ) VALUES(1,'2','02-02-2016');")->fetchColumn();
-		// $db->exec("INSERT INTO probando ( id, cont, fecha ) VALUES(10,'2','02-02-2016')");
-
-		// $datos = $db->prepare("INSERT INTO probando ( id, cont, fecha ) VALUES(100,'2','01/01/1999'),(100,'2','01/01/1999')");
-		// $datos->execute();
-		$query="INSERT INTO probando ( id, cont, fecha )";
-
-		for ($i=0; $i < $len; $i++) { 
-			if(($i+1)!=$len){
-				$query.=" SELECT $i,'2','01/01/1999' UNION ALL ";
-			}else{
-				$query.=" SELECT $i,'2','01/01/1999'";				
-			}
-
-		}
-		$datos = $db->prepare($query);
-		$datos->execute();
-
-
-		// $query = "INSERT INTO probando VALUES('$id','$i','$fecha')";
-		// $db->query($query);
-		// $result = $sth->fetchAll();
+		// if($len>0){
+		// 	$datos = $db->prepare($query);
+		// 	$datos->execute();
+		// }
 
 
 
-		// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
-		// $datos->execute(array(':id' => $id, ':cont' => $cont, ':fecha' => $fecha));
-
-
-
-		echo json_encode("Done.");
+		echo json_encode("Done Detalle.".$cont);
 	}else{
 		echo json_encode("NOT Done.");
 
@@ -221,30 +128,54 @@ switch ($info) {
 
 
 
-	// case "maestro":
-	// $verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
-	// if($verificar > 0)
-	// {
-	// 	$datos = $db->prepare("SELECT a.TipoDcto TipoDcto,a.NroDcto NroDcto,a.Fecha Fecha,a.FechaVto FechaVto,'Saldo deuda' Obs,a.CodCliente CodCliente,count(*) Conteo
-	// 		from maestro a inner join  detalle b on a.tipodcto=b.tipodctom and a.nrodcto=b.nrodctom and codconcepto=1400
-	// 		group by a.TipoDcto,a.NroDcto,a.Fecha,a.FechaVto,a.CodCliente
-	// 		order by tipodcto,nrodcto");
+	case "maestro":
+	
+	$verificar = $db->query("SELECT count(*) FROM Personal WHERE CodPersonal= '$codigo' and CI='$password'")->fetchColumn();
+	if($verificar > 0)
+	{
+		$id = $db->query("SELECT max(CodSincMaestro) FROM SincMaestro")->fetchColumn();
+		if($id == ''){
+			$id = 1;
+		}else{
+			$id = $id + 1;
+		}
 
-	// 	$datos->execute();
+		$cont=0;
 
-	// 	$articulos='';
-	// 	while($row = $datos->fetch()) {
-	// 		for ($i=0; $i < 7; $i++) { 
-	// 			$articulos.=$row[$i].'@';
-	// 		}
-	// 	}
-	// 	$array  = substr($articulos, 0, -1);
-	// 	$array1 = explode("@", $array);
-	// 	$array2 = array_chunk($array1, 7);
+		$array=$data;
+		$array  = substr($array, 0, -1);
+		$array1 = explode("|", $array);
+		if( !empty( $array1 ) ){
+			$array2 = array_chunk($array1, 7);
+			// $cont=count($array2);
+			foreach ($array2 as $value) {
 
-	// 	echo json_encode($array2);
-	// }
-	// break;
+				$aux=explode(' ', $value[2]);
+				$value[2]=$aux[0];
+				$value[2]=implode('-', array_reverse(explode('-', $value[2])));
+
+				$aux=explode(' ', $value[3]);
+				$value[3]=$aux[0];
+				$value[3]=implode('-', array_reverse(explode('-', $value[3])));
+
+				// $cont=$value[3];
+				// $datos = $db->prepare('INSERT INTO probando VALUES(:id, :cont, :fecha)');
+				// $datos->execute(array(':id' => $id, ':cont' => $cont, ':fecha' => $fecha));
+
+				$datos = $db->prepare('INSERT INTO SincMaestro VALUES(:CodSincMaestro, :TipoDcto, :NroDcto, :Fecha, :FechaVto, :Obs, :CodCliente, :Conteo, :FechaCarga)');
+				$datos->execute(array('CodSincMaestro' => $id,'TipoDcto' => $value[0], 'NroDcto' => $value[1], 'Fecha' => $value[2], 'FechaVto' => $value[3], 'Obs' => $value[4], 'CodCliente' => $value[5], 'Conteo' => $value[6], 'FechaCarga' => $fecha));
+				
+			}
+		}
+		$cont=count($array2);
+
+		echo json_encode("Done Maestro.".$cont);
+	}else{
+		echo json_encode("NOT Done.");
+
+	}
+
+	break;
 
 
 
